04b6fc5a3b38a15620823e85e78cc836decb7dc5
Takuya Yamada <yamada.takuya@ais.cmc.osaka-u.ac.jp>
2017-02-01 14:42:02 +0900

    不要なシリアライザ削除

--- server/serializer.py
+++ server/serializer.py
@@ -1,24 +1,15 @@
-import json
 import logging
 
 from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework import serializers
 
-from .models import Agent, App, Node, NodeType
+from .models import Node, NodeType
 
 logger = logging.getLogger(__name__)
 
 
-class AppSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = App
-        fields = ('name', 'user')
-
-
 class NodeSerializer(serializers.ModelSerializer):
-    # 組み込み関数のtypeと識別子名が衝突してる
-    # あとで直すか？
     args_str = serializers.CharField(max_length=256, required=False)
     type = serializers.CharField(max_length=128)
     to = serializers.ListField(
@@ -44,7 +35,6 @@ class NodeSerializer(serializers.ModelSerializer):
         except ObjectDoesNotExist:
             print("object does not exist")
 
-
     def create(self, validated_data):
         if 'args_str' in validated_data:
             node = Node.objects.create(
@@ -57,46 +47,3 @@ class NodeSerializer(serializers.ModelSerializer):
         self.get_nodetype_from_name(
                 node, self.validated_data.pop("type"))
         return node
-
-
-class NodeTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NodeType
-        fields = ('name')
-
-
-class AgentSerializer(serializers.ModelSerializer):
-    nodes = serializers.ListField(child=serializers.CharField(max_length=128))
-
-    class Meta:
-        model = Agent
-        fields = ('user_id', 'agent_id', 'longitude',
-                  'latitude', 'busy', 'nodes', 'ip_addr')
-
-    def create(self, validated_data):
-        nodes_data = self.validated_data.pop("nodes")
-        agent = Agent.objects.create(**self.validated_data)
-        self.nodes_create_and_add(agent, nodes_data)
-        return agent
-
-    def update(self, agent, validated_data):
-        agent.user_id = validated_data.get('user_id', agent.user_id)
-        agent.agent_id = validated_data.get('agent_id', agent.agent_id)
-        agent.longitude = validated_data.get('longitude', agent.longitude)
-        agent.latitude = validated_data.get('latitude', agent.latitude)
-        agent.busy = validated_data.get('busy', agent.busy)
-        agent.ip_addr = validated_data.get('ip_addr', agent.ip_addr)
-
-        nodes_data = self.validated_data.pop("nodes")
-        self.nodes_create_and_add(agent, nodes_data)
-
-        agent.save()
-        return agent
-
-    def nodes_create_and_add(self, agent, nodes_data):
-        agent.available_node_types.clear()
-        for node_data in nodes_data:
-            node_type, created = NodeType.objects.\
-                    get_or_create(name=str(node_data))
-            node_type.save()
-            agent.available_node_types.add(node_type)
