36ded9776d85408b067f132dfbf047ddaf3fb540
Takuya Yamada <yamada.takuya@ais.cmc.osaka-u.ac.jp>
2017-02-14 19:42:54 +0900

    大本のurlpatternから参照するように

--- seot_server/urls.py
+++ seot_server/urls.py
@@ -21,6 +21,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^', include('server.urls.ui_urls')),
-    url(r'^api/', include('server.urls.api_urls')),
+    url(r'^', include('server.urls.app')),
+    url(r'^api/', include('server.urls.api')),
 ]
